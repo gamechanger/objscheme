30bd70790419a6cb6b51db3bc3f1255ca0ddc358
Kiril Savino <kiril@gamechanger.io>
2012-12-03 15:16:01 -0500

    being altogether too clever

--- ObjScheme/SchemeBacked.m
+++ ObjScheme/SchemeBacked.m
@@ -11,4 +11,74 @@
 
 @implementation SchemeBacked
 
+// -- static stuff -- //
+
++ (NSString*)toSchemeName:(NSString*)cocoaName {
+  NSRegularExpression* finalP = [NSRegularExpression regularExpressionWithPattern: @"_p$"
+                                                                          options: 0
+                                                                            error: nil];
+  NSString* schemeName = [finalP stringByReplacingMatchesInString: cocoaName
+                                                          options: 0
+                                                            range: NSMakeRange(0, [cocoaName length])
+                                                     withTemplate: @"?"];
+  return [schemeName stringByReplacingOccurrencesOfString: @"_" withString: @"-"];
+}
+
++ (NSString*)toCocoaName:(NSString*)schemeName {
+  NSRegularExpression* finalP = [NSRegularExpression regularExpressionWithPattern: @"\\?$"
+                                                                          options: 0
+                                                                            error: nil];
+  NSString* cocoaName = [finalP stringByReplacingMatchesInString: schemeName
+                                                         options: 0
+                                                           range: NSMakeRange(0, [schemeName length])
+                                                    withTemplate: @"_p"];
+  return [cocoaName stringByReplacingOccurrencesOfString: @"-" withString: @"_"];
+}
+
+// -- object methods below here -- //
+
+- (id)initWithScope:(ObSScope*)scope {
+  if ( ( self = [super init] ) ) {
+    _scope = [scope retain];
+  }
+  return self;
+}
+
+- (NSMethodSignature*)methodSignatureForSelector:(SEL)selector {
+  NSMethodSignature* ret = [super methodSignatureForSelector: selector];
+  if ( ret != nil ) {
+    return ret;
+  }
+
+  NSString* schemeName = [SchemeBacked toCocoaName: NSStringFromSelector(selector)];
+  NSDictionary* environ = _scope.environ;
+  if ( [environ objectForKey: schemeName] ) {
+    /*
+      Return type is object (id) / ptr.
+     |
+     |  self (implied) is a ptr
+     | |
+     | |  selector (implied)
+     | | |
+     | | | takes 1 param, an NSArray*
+     | | | |
+     v v v v
+     @ @ : @
+    */
+    return [NSMethodSignature signatureWithObjCTypes: "@@:@"];
+  }
+
+  return nil;
+}
+
+- (void)forwardInvocation:(NSInvocation*)invocation {
+  NSInteger firstArgumentIndex = 2; // 0 = self, 1 = selector
+  NSArray* args = NULL;
+  [invocation getArgument: &args atIndex: firstArgumentIndex];
+  ObSCons* schemeArgs = [ObjScheme list: args];
+  NSString* schemeFunctionName = [SchemeBacked toSchemeName: NSStringFromSelector(invocation.selector)];
+  NSArray* schemeCall = [NSArray arrayWithObjects: schemeFunctionName, schemeArgs, nil];
+  [invocation setReturnValue: [_scope evaluate: [ObjScheme list: schemeCall]]];
+}
+
 @end
