f35507f005f9d2d967c2b228894cb4c3500a19b6
paetling <paetling@gmail.com>
2015-03-09 15:38:04 -0400

    add in a string length function

--- ObjScheme/ObSStrings.m
+++ ObjScheme/ObSStrings.m
@@ -45,6 +45,13 @@
         NSLog(@"%@", x);
         return [ObjScheme unspecified];
       }]];
+  
+  
+  [scope defineFunction: [ObSNativeUnaryLambda named: SY(@"string-length")
+                                      fromBlock: ^(id x) {
+        NSString *string = ((NSString *)x);
+        return [NSNumber numberWithInteger:string.length];
+      }]];
 
   [scope defineFunction: [ObSNativeLambda named: SY(@"format")
                                       fromBlock: ^(ObSCons* args) {
